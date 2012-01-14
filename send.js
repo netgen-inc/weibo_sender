a4bf332687935ad742083c44692833d1358a9997
iamlaobie <iamlaobie@gmail.com>
2012-01-14 17:36:35 +0800

    1、重构代码 2、增加转发和发图片的功能

--- send.js
+++ send.js
@@ -1,167 +1,195 @@
 var fs = require('fs');
-var settings = JSON.parse(fs.readFileSync(__dirname + '/etc/settings.json', 'utf8'));
+var settings = require(__dirname + '/etc/settings.json');
 var url = require('url');
 var de = require('devent').createDEvent('sender');
 var queue = require('queuer');
 var logger = require('./lib/logger').logger(settings.logFile);
 var util = require('util');
 var event = require('events').EventEmitter;
-var mysql = require('mysql');
-var myCli;
 
-//删除队列的API
+//发送队列的API
 var sendQ = queue.getQueue('http://'+settings.queue.host+':'+settings.queue.port+'/queue', settings.queue.send);
 
-//新浪微博的API
-var weibo = new require('./lib/sina').weibo;
-weibo.init(settings);
+var Sender = require('./lib/sender').Sender;
 
-process.on('SIGUSR2', function () {
-    settings = JSON.parse(fs.readFileSync(__dirname + '/etc/settings.json', 'utf8'));
-    weibo.init(settings);
+//发送对象保存在该数组中
+var senders = [];
+
+var db = require('./lib/db').db;
+db.init(settings);
+
+//所有微博账号
+var weiboAccounts = {};
+db.loadAccounts(function(err, accounts){
+    if(err){
+        console.log('!!!load account error!!!');   
+        return;
+    }
+    weiboAccounts = accounts;
+    console.log('access token loaded');
+    //由于发送依赖账号，所以必须先加载完账号才能开始处理发送请求
+    console.log('starting dequeue');
+    start();
 });
 
-//初始化mysql客户端
-myCli = mysql.createClient(settings.mysql);
-myCli.query('use ' + settings.mysql.database);
-myCli.query('set names utf8');
-
-Date.prototype.getStamp = function(){
-    var time = this.getTime();   
-    return parseInt(time / 1000);
+var taskBack = function(task,  status){
+    if(status){
+        de.emit('task-finished', task);  
+    }else{
+        de.emit('task-error', task);     
+    }
 }
 
-de.on('queued', function( queue ){
-    if(queue == settings.queue.send){
-        console.log( queue + "有内容");
-        dequeue();
-    }
-});
-
-/*
-setTimeout(function(){
-    var blog = {stock_code:'sz900001', content:'abc'};   
-    var sender = new Sender();
-    var task = { queue: 'weibo_send',
-  uri: 'mysql://172.16.39.117:3306/webio_send?url#1',
-  retry: 1 };
-    sender.send(task);
-    weibo.send(blog, function(statusCode, body){
-       console.log([statusCode, body]);
-    });
-}, 1000);
-*/
-
-
-setInterval(function(){
-    dequeue();    
-}, settings.queue.interval);
-
-var senders = [];
 var dequeue = function(){
-    if(senders.length == 0){
-        return;   
+    for(var i = 0; i < senders.length; i++){
+        if(settings.mode == 'debug'){
+            console.log('running status--'+ i + '--'+ senders[i].running);
+        }
+        
+        if(senders[i].running){
+            continue;   
+        }
+        (function(sender){
+            sender.running = true;
+            sendQ.dequeue(function(err, task){
+                if(err == 'empty' || task == undefined){
+                    sender.running = false;
+                    console.log('send queue is empty');
+                    return;
+                }
+                console.log(['dequeue', task]);
+                send(task, sender, {task:task});
+            });
+        })(senders[i]);
     }
-    sendQ.dequeue(function(err, task){
-        if(err == 'empty' || task == undefined){
-            console.log('send queue is empty');
+}
+
+var start = function(){
+    setInterval(function(){
+        dequeue();    
+    }, settings.queue.interval);  
+    
+    de.on('queued', function( queue ){
+        if(queue == settings.queue.send){
+            console.log( queue + "有内容");
+            dequeue();
+        }
+    }); 
+    console.log('sender start ok'); 
+}
+
+
+var send = function(task, sender, context){
+    db.getBlogByUri(task.uri, function(err, results){
+        if(err || results.length == 0){
+            logger.info("error\tNot found the resource:" + task.uri);
+            sender.running = false;
+            return; 
+        }
+        
+        blog = results[0];
+        
+        //debug模式下，总是使用stock0@netgen.com.cn发送微博
+        if(settings.mode == 'debug'){
+            blog.stock_code = 'sz900000';    
+        }
+        
+        //微博账号错误
+        if(!weiboAccounts[blog.stock_code] || 
+            !weiboAccounts[blog.stock_code].access_token || 
+            !weiboAccounts[blog.stock_code].access_token_secret){
+            logger.info("error\t" + blog.id + "\t" + blog.stock_code + "\tNOT Found the account\t"); 
+            sender.running = false;
+            taskBack(task, true);
             return;
         }
-        console.log(task);
-        var sender = senders.pop();
-        if(!sender){
-            de.emit('task-error', task);
-        }else{
-            sender.send(task);
-        }
+        sender.send(blog, weiboAccounts[blog.stock_code], context);
     });
+};
+
+
+/**
+ 发送结束后的处理，返回true表示发送完成
+*/
+var complete = function(error, body, blog, task){
+    if(!error){
+        logger.info("success\t" + blog.id + "\t" + blog.stock_code + "\t" + blog.content + "\t" + body.id + "\t" + body.t_url);
+        db.sendSuccess(blog, body.id);
+        return true;
+    }
+    
+    var errMsg = error.error;
+    logger.info("error\t" + blog.id +"\t"+ blog.stock_code + "\t" + errMsg);  
+    
+    //发送受限制
+    if(errMsg.match(/^40(308|090)/)){
+        return false;
+    //40013太长, 40025重复
+    }else if(errMsg.match(/^400(13|25)/)){                                                                                                                          
+        return true;
+    }else{
+        if(task.retry >= settings.queue.retry){
+            logger.info("error\t" + blog.id +"\t"+ blog.stock_code + "\t"+ "\tretry count more than "+settings.queue.retry);
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
-//记录股票最后下发时间，用于控制下发频率
-var sent = {};
-var Sender = function(){
-    var _self = this;
-    this.send = function(task){
-        _self.getBlogByUri(task.uri, function(err, results){
-            if(err || results.length == 0){
-               logger.info("Not found the resource:" + task.uri);
-               de.emit('task-finished', task); 
-               _self.emit('end');
-               return;
-            }
-            var blog = results[0];
-            blog.stock_code = 'sz900000';
-            //发送间隔太短
-/*
-            if(!_self.requestAble(blog.stock_code)){
-                console.log('rate limit');
-                de.emit('task-error', task);
-                _self.emit('end');
-                return;
-            }
-*/
-            blog.task = task;
-            weibo.send(blog, function(statusCode, body){
-                sent[blog.stock_code] = parseInt(new Date().getTime());
-                if(statusCode != 200){
-                    //发送受限制
-                    if(body.error.match(/^40(308|090)/)){
-                        console.log('too fast');
-                        de.emit('task-error', task);  
-                    //40013太长, 40025重复
-                    }else if(body.error.match(/^400(13|25)/)){
-                        de.emit('task-finished', task);                                                                                                                           
-                        logger.info("Send error\t" + statusCode +"\t" + body.error + "\t"+ task.uri);  
-                    }else{
-                        if(task.retry >= 5){
-                            de.emit('task-finished', task);
-                            logger.info("Send error\tretry count > 5\t" + statusCode +"\t" + body.error + "\t"+ task.uri);
-                        }else{
-                            logger.info("Send error\t" + statusCode +"\t" + body.error + "\t"+ task.uri);
-                            de.emit('task-error', task);  
-                        }
-                    }
-                }else{
-                    _self.sendSuccess(blog, body.id, blog.stock_code);
-                    de.emit('task-finished', task);
-                }
-                _self.emit('end');
-            });
-        });
-    };
-    
-    this.getBlogByUri = function(uri, cb){
-        var uri = url.parse(uri);
-        var id = uri.hash.substring(1);
-        var sql = "select * from micro_blog where id = '" + id + "' AND send_time = 0";
-        myCli.query(sql, function(err, results, fields){
-            cb.call(null, err, results);
-        });
-    };
-    
-    this.sendSuccess = function(blog, sinaId, stockCode){
-        var time = new Date().getTime();
-        time = time.toString().substring(0, 10);
-        var sql = "update micro_blog SET send_time = '"+time+"', status = 1 WHERE id = '"+blog.id+"'";
-        myCli.query(sql);
-        
-        var sql = "INSERT INTO sent_micro_blog(micro_blog_id, weibo_id, send_time, stock_code) values("+blog.id+", "+sinaId+", "+time+", '"+stockCode+"')";
-        myCli.query(sql);
-    }
-    
-    this.requestAble = function (stockCode){
-        if(sent[stockCode] && (parseInt(new Date().getTime()) - sent[stockCode]) < settings.weibo.interval * 1000){ 
-            return false; 
-        }
-        return true; 
-    }
-};
-util.inherits(Sender, event); 
-for(i = 0; i < 2; i++){
+
+for(i = 0; i < settings.sendersCount; i++){
     var sender = new Sender();
-    sender.on('end', function(){
-	    senders.push(sender);
-    })
+    sender.init(settings);
+    (function(s){
+        s.on('send', function(error, body, blog, context){
+            s.running = false;
+            taskBack(context.task, complete(error, body, blog, context.task));
+            dequeue();
+        })    
+    })(sender);
     senders.push(sender);
 }
+
 fs.writeFileSync(__dirname + '/server.pid', process.pid.toString(), 'ascii');
+
+//收到进程信号重新初始化
+process.on('SIGUSR2', function () {
+    settings = require('./etc/settings.json');
+    db.init(settings);
+    for(i = 0; i < senders.length; i++){
+        senders[i].init(settings);
+    }
+});
+
+/**
+ * 测试代码
+setTimeout(function(){
+    var sender = new Sender();
+    sender.init(settings);
+    var task = {uri:'mysql://abc.com/stock_radar#1'};
+    sender.on('send', function(error, body, blog, context){
+        console.log(error);
+        taskBack(context.task, complete(error, body, blog));
+    });
+    send(task, sender, {task:task});
+}, 1000);
+
+setTimeout(function(){
+    var sender = new Sender();
+    sender.init(settings);
+    var task = {uri:'mysql://abc.com/stock_radar#1'};
+    sender.on('repost', function(error, body, id, status, context){
+        console.log([error, body, id, status, context]);
+        //taskBack(context.task, complete(error, body, blog));
+    });
+    sender.repost('3401770199850919', 'abc', weiboAccounts.sz900000, {abc:'abc'});
+}, 1000);
+
+
+
+*/
+
+
+
+ 
