94d77dc233614194377fc015ee74fbf73464ee85
liujun <liujun@netgen.com.cn>
2012-04-16 19:16:32 +0800

    开发粉丝状态抓取程序

--- cron/fans.js
+++ cron/fans.js
@@ -5,13 +5,17 @@ var weibo = require('weibo');
 var async = require('async');
 var db = require('../lib/db').db;
 db.init(settings);
+
+var dbStat = require('../lib/db_stats').db;
+dbStat.init(settings);
+
 var cli = mysql.createClient(settings.mysql);
 
 weibo.init('tsina', settings.weibo.appkey, settings.weibo.secret);
 var accounts;
 
-//从api中读取粉丝数并写入数据库
-var fans = function(task, callback){
+//fetch fans count
+var fansCount = function(task, callback){
     var user = task.user;
     weibo.tapi.user_show(task, function(err, result){
         if(err){
@@ -32,6 +36,48 @@ var fans = function(task, callback){
     });
 }
 
+
+var fansList = function(task, queueCallback){
+    var localUser = task.user;
+    weibo.tapi.followers(task, function(err, result){
+        if(err || result.length == 0){
+            console.log([err, result]);
+            queueCallback();
+            return;
+        }
+        if(result.next_cursor && result.next_cursor > 0){
+            task.user = localUser;
+            task.cursor = result.next_cursor;
+            lq.push(task);
+        }
+
+        async.forEach(result.users, function(user, callback){
+            user.weibo_user_id = user.id.toString();
+            user.account_id = localUser.id;
+            user.last_send_weibo = '0000-00-00 00:00:00';
+            user.created_at = tool.getDateString(new Date(user.created_at));
+
+            if(user.status){
+                user.last_send_weibo = tool.getDateString(new Date(user.status.created_at));
+            }
+
+            delete user.id;
+            dbStat.insertFollowerInfo(user, function(err, user){
+                if(err){
+                    console.log(err);
+                    return;
+                }
+                dbStat.insertFollowerStatus(user);
+                callback();
+            });
+
+        }, function(){
+            queueCallback();
+        });
+
+    });
+}
+
 var insert = function(stock, cnt, accountId, cb){
     var d = tool.getDateString(null, false);
     var sql = "INSERT INTO followers (stock_code, stat_date, cnt, account_id) VALUES(?, ?, ?, ?) " + 
@@ -42,24 +88,28 @@ var insert = function(stock, cnt, accountId, cb){
         }
         cb(err);
     });
-}
+};
 
-var q = async.queue(fans, 5);
+var cq = async.queue(fansCount, 5);
+var lq = async.queue(fansList, 5);
 
-//加载账号
 db.loadAccounts(function(err, accs){
     accounts = accs;
     for(var stock in accounts){
         if(!accounts[stock].weibo_user_id){
             continue;   
         }
-        var task = {user_id:accounts[stock].weibo_user_id, user:accounts[stock]};
-        q.push(task);   
+
+        var taskCount = {user_id:accounts[stock].weibo_user_id, user:accounts[stock]};
+        cq.push(taskCount);   
+
+        var taskList = {user_id:accounts[stock].weibo_user_id, user:accounts[stock],cursor:-1, count:200};
+        lq.push(taskList);
     }
 });
 
 setInterval(function(){
-    console.log(q.length());
+    console.log(lq.length());
 }, 1000);
 
 
