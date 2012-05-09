a44b5533506bd706d38d4db35201d8f2e8c6ecee
liujun <liujun@netgen.com.cn>
2012-05-09 16:48:14 +0800

    add account mention script

---
+++ cron/account_mention.js
@@ -0,0 +1,99 @@
+var settings = require('../etc/settings.json');
+var tool = require('../lib/tool').tool;
+var weibo = require('weibo');
+var async = require('async');
+var db = require('../lib/db').db;
+db.init(settings);
+
+var dbStat = require('../lib/db_stats').db;
+dbStat.init(settings);
+
+
+weibo.init('tsina', settings.weibo.appkey, settings.weibo.secret);
+
+var fetchMentionList = function(task, queueCallback){
+    var timer = setTimeout(function(){
+        queueCallback();
+    }, 30000);
+    var localUser = task.user;
+    weibo.tapi.mentions(task, function(err, result){
+        clearTimeout(timer);
+        if(err){
+            if(task.retry < 5){
+                task.retry += 1;
+                task.user = localUser;
+                if(err.message.match(/^40312/)){
+                    setTimeout(function(){
+                        lq.push(task);
+                    }, 60000);    
+                }else{
+                    lq.push(task);
+                }
+                
+            }
+            console.log(err);
+        }
+        if(err || result.length == 0){
+            queueCallback();
+            return;
+        }
+
+        var duplicated = false;
+        var q = async.queue(function(mention, sCallback){
+            if(duplicated){
+                sCallback();
+                return;
+            }
+
+            mention.account_id = localUser.id;
+            dbStat.insertMention(mention, function(err, info){
+                if(err && err.number == 1062){
+                    duplicated = true;
+                }
+                sCallback();
+            });
+        }, 1);
+
+        q.drain = function(){
+            if(!duplicated){
+                task.user = localUser;
+                task.page += 1;
+                task.retry = 0;
+                lq.push(task);
+            }
+            queueCallback();
+        }
+        async.forEach(result, function(mention, callback){
+            q.push(mention);
+            callback();
+        });
+    });
+}
+
+
+
+var lq = async.queue(fetchMentionList, 5);
+lq.drain = function(){
+    console.log("complete!!!");
+    setTimeout(function(){
+        proecess.exit(0);
+    }, 1000 * 60);
+}
+
+db.loadAccounts(function(err, accs){
+    accounts = accs;
+    for(var stock in accounts){
+        if(!accounts[stock].weibo_user_id){
+            continue;   
+        }
+
+        var listTask = {user_id:accounts[stock].weibo_user_id, user:accounts[stock], page:1, count:200,retry:0};
+        lq.push(listTask);
+    }
+});
+
+
+setInterval(function(){
+    var o = {listQueue:lq.length()}
+    console.log(o);
+}, 1000);
